1eac440a18ff8aae3f1025e5efb9bb14af2e6299
thanharrow <thanhngch.aws@gmail.com>
2024-12-09 16:35:00 +0700

    update new version

--- app/src/main/java/org/kauthara/chamrumi/MainActivity.java
+++ app/src/main/java/org/kauthara/chamrumi/MainActivity.java
@@ -2,7 +2,7 @@ package org.kauthara.chamrumi;
 
 import android.content.Intent;
 import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
